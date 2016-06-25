7c1a9a9ab0af907a52083ec58a103f46baf26bdb
Sahana Rajasekar <shnraj@gmail.com>
2016-06-25 13:33:23 -0700

    Add timer

--- static/js/books.js
+++ static/js/books.js
@@ -1,1 +1,17 @@
+var started = false;
 
+function start() {
+    if (!started) {
+        started = true;
+        setInterval(countTimer, 1000);
+        var totalSeconds = 0;
+        function countTimer() {
+            ++totalSeconds;
+            var hour = Math.floor(totalSeconds /3600);
+            var minute = Math.floor((totalSeconds - hour*3600)/60);
+            var seconds = totalSeconds - (hour*3600 + minute*60);
+
+            document.getElementById("timer").innerHTML = minute + ":" + seconds;
+        };
+    }
+}
