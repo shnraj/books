b8970f7aab224d5d5746b963ceaec3f68926ea32
Sahana Rajasekar <shnraj@gmail.com>
2016-07-08 14:51:24 -0700

    All book reading times

--- static/js/books.js
+++ static/js/books.js
@@ -27,17 +27,10 @@ function start() {
 }
 
 function stop() {
-    fiction_books = [{'isbn': '9780804141260', 'name': 'VINEGAR GIRL', 'author': 'Anne Tyler', 'image': 'https://s1.nyt.com/du/books/images/9780804141260.jpg', 'amazon_url': 'http://www.amazon.com/Vinegar-Girl-Novel-Hogarth-Shakespeare/dp/0804141266?tag=thenewyorktim-20', 'pages': 240}, {'isbn': '9780812996494', 'name': 'A HERO OF FRANCE', 'author': 'Alan Furst', 'image': 'https://s1.nyt.com/du/books/images/9780812996500.jpg', 'amazon_url': 'http://www.amazon.com/Hero-France-Novel-Alan-Furst-ebook/dp/B018CHH2A8?tag=thenewyorktim-20', 'pages': 256}]
     started = false;
     clearInterval(timeVar);
     document.getElementById("wpm").innerHTML = 'Words per minute: ' + wpm(343, totalSeconds);
-    for (var i=0; i < fiction_books.length; i++) {
-        className = "timeToRead" + fiction_books[i].isbn;
-        var total_min = fiction_books[i].pages * 225 / wpm(343, totalSeconds);
-        var hours = Math.floor((total_min)/60);
-        var min =  Math.floor(total_min - (hours * 60));
-        document.getElementById(className).innerHTML = hours + ' hr ' + min + ' min';
-    }
+    update_book_times()
     totalSeconds = 0;
 }
 
@@ -49,4 +42,17 @@ function wpm(words, time_in_sec) {
     var min = time_in_sec/60;
     var wpm = words/min;
     return Math.round(wpm * 100) / 100
+}
+
+function update_book_times() {
+    var lists = [fiction_books, nonfiction_books];
+    for (var j=0; j < lists.length; j++) {
+        for (var i=0; i < lists[j].length; i++) {
+            className = "timeToRead" + lists[j][i].isbn;
+            var total_min = lists[j][i].pages * 225 / wpm(343, totalSeconds);
+            var hours = Math.floor((total_min)/60);
+            var min =  Math.floor(total_min - (hours * 60));
+            document.getElementById(className).innerHTML = hours + ' hr ' + min + ' min';
+        }
+    }
 }
