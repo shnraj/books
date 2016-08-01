56aabca09eeccc656d55f753f23fce787f048fe6
Sahana Rajasekar <shnraj@gmail.com>
2016-08-01 16:30:58 -0700

    Add js for each book page

---
+++ static/js/books_page.js
@@ -0,0 +1,17 @@
+var books;
+
+function setup() {
+    var cookie_str = document.cookie;
+    var wpm = cookie_str.substring(cookie_str.indexOf('=') + 1);
+    update_book_times(wpm);
+}
+
+function update_book_times(personal_wpm) {
+    for (var i=0; i < books.length; i++) {
+        className = "timeToRead" + books[i].isbn;
+        var total_min = books[i].pages * 225 / personal_wpm;
+        var hours = Math.floor((total_min)/60);
+        var min =  Math.floor(total_min - (hours * 60));
+        document.getElementById(className).innerHTML = hours + ' hr ' + min + ' min';
+    }
+}
